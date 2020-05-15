6c4cf6a4e073ca4c595b3eb399785d33f4a328d2
Ermin Obradovac <au592117@uni.au.dk>
2020-05-15 12:13:32 +0200

    repo virker ikke,mangler dbcontext - kommer fra Lau og Osk

---
+++ Vejrstation/Repository/Repository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Vejrstation.Interfaces;
+
+namespace Vejrstation.Repository
+{
+    public class Repository<TEntity>: IRepository<TEntity> where TEntity: class
+    {
+        public virtual TEntity Read(int id)
+        {
+            //return Context.Set<TEntity>().Find(id);
+        }
+
+        public virtual void Create(TEntity entity)
+        {
+            //Context.Set<TEntity>().Add(entity);
+        }
+    }
+}
