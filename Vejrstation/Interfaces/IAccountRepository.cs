a4e86e3c2337bbf9e1a9f8fd4d640223ea368e40
SBrahe <201807173@post.au.dk>
2020-05-15 16:28:38 +0200

    .

--- Vejrstation/Interfaces/IAccountRepository.cs
+++ Vejrstation/Interfaces/IAccountRepository.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
 using Vejrstation.Controllers;
 using Vejrstation.Entities;
 
--- Vejrstation/Repository/AccountRepository.cs
+++ Vejrstation/Repository/AccountRepository.cs
@@ -1,4 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
+using Vejrstation.Data;
 using Vejrstation.Entities;
 using Vejrstation.Interfaces;
 
@@ -6,10 +10,11 @@ namespace Vejrstation.Repository
 {
     public class AccountRepository : Repository<Account>, IAccountRepository
     {
-        public AccountRepository(DbContext context) : base(context)
+       
+        public AccountRepository(WeatherServerDbContext context) : base(context)
         {
+            
         }
-        
-        
+
     }
 }
